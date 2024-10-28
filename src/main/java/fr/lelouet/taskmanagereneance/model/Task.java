5814d20adc8d0b7f8b6db433ea09dc5bd150b7cf
alelouet <alelouet@coreoz.com>
2024-10-28 12:23:14 +0100

    add Task bean

---
+++ src/main/java/fr/lelouet/taskmanagereneance/model/Task.java
@@ -0,0 +1,35 @@
+package fr.lelouet.taskmanagereneance.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@Entity
+public class Task {
+
+    @jakarta.persistence.Id
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String label;
+    private String category; // TODO alelouet : relier la catégory avec les futures catégories dispo pour les users (BDD)
+    private LocalDate dueDate;
+    private boolean done;
+
+    // TODO alelouet : ajouter dates crea/modif
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = true) // TODO alelouet : Faire un choix sur la possibilité d'avoir des taches sans user
+    private User user;
+
+}
