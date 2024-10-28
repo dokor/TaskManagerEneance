c17eccc30c6420d4e538aaf356ddace162941be0
alelouet <alelouet@coreoz.com>
2024-10-28 14:44:52 +0100

    add default service/controller User

---
+++ src/main/java/fr/lelouet/taskmanagereneance/controller/UserController.java
@@ -0,0 +1,24 @@
+package fr.lelouet.taskmanagereneance.controller;
+
+import fr.lelouet.taskmanagereneance.model.User;
+import fr.lelouet.taskmanagereneance.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<User> registerUser(@RequestBody User user) {
+        User newUser = userService.registerUser(user);
+        return ResponseEntity.ok(newUser);
+    }
+}
