c5d0f84a21e30ac4224a6fdb0100a0689dab4d84
alelouet <alelouet@coreoz.com>
2024-10-28 15:33:51 +0100

    maj default authentification

--- src/main/java/fr/lelouet/taskmanagereneance/config/SecurityConfig.java
+++ src/main/java/fr/lelouet/taskmanagereneance/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -12,6 +13,8 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+// Active la sécurité au niveau des méthodes plutot que de les gérer unitairement
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
 
     @Bean
@@ -19,7 +22,6 @@ public class SecurityConfig {
         http
             .csrf().disable()
             .authorizeRequests()
-            .requestMatchers("/register", "/login").permitAll()
             .anyRequest().authenticated()
             .and()
             .formLogin();
