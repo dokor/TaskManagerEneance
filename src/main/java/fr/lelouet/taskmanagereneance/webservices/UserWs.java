b668b2a1af37996e7a131e73965a9ddb24a1451e
alelouet <alelouet@coreoz.com>
2024-10-28 15:34:08 +0100

    add security authent on UserWs

--- src/main/java/fr/lelouet/taskmanagereneance/webservices/UserWs.java
+++ src/main/java/fr/lelouet/taskmanagereneance/webservices/UserWs.java
@@ -6,6 +6,7 @@ import fr.lelouet.taskmanagereneance.webservices.enums.WsError;
 import fr.lelouet.taskmanagereneance.webservices.utils.EneanceException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,24 +20,25 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/users")
+@PreAuthorize("isAuthenticated()") // Par défault, le WS demande une authentification
 public class UserWs {
 
     @Autowired
     private UserController userController;
 
     @PostMapping("/register")
+    @PreAuthorize("permitAll()") // Accessible sans authentification
     public ResponseEntity<User> registerUser(@RequestBody User user) {
         try {
             User newUser = userController.registerUser(user);
-            // TODO alelouet : Ne pas renvoyer le password, renvoyer un bean réduit
+            // TODO alelouet : Ne pas renvoyer le password, renvoyer un bean réduit car WS non authenth
             return ResponseEntity.ok(newUser);
         } catch (Exception e) {
-            // On ne donne volontairement pas d'informations au front
+            // On ne donne volontairement pas d'informations sur l'erreur au front
             throw new EneanceException(WsError.USER_CANT_BE_CREATED);
         }
     }
 
-    // TODO alelouet : a déplacer vers un WS authentifié.
     @GetMapping("/{id}")
     public ResponseEntity<User> getUser(@PathVariable Long id) {
         return userController.findById(id)
