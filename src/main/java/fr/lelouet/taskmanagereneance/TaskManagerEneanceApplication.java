5b928c0c5b4a2171034c7b0525e701ff1d50244c
alelouet <alelouet@coreoz.com>
2024-10-28 11:37:28 +0100

    init project with spring initializer

---
+++ src/main/java/fr/lelouet/taskmanagereneance/TaskManagerEneanceApplication.java
@@ -0,0 +1,13 @@
+package fr.lelouet.taskmanagereneance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagerEneanceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskManagerEneanceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/lelouet/taskmanagereneance/TaskManagerEneanceApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.lelouet.taskmanagereneance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerEneanceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
